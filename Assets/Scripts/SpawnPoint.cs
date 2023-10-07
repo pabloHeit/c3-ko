4f7ed141a61385ce9956043cf24e89363fb7526e
pabloHeit <pabloaheit@yahoo.com.ar>
2023-10-07 03:11:06 -0300

    Ritmo por archivo de texto
    Se puede agregar el ritmo de la canción por archivo de texto

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -9,33 +9,56 @@ public class SpawnPoint : MonoBehaviour
 {
     [SerializeField] GameObject nota;
     [SerializeField] float Timer;
-    [SerializeField] int index =1;
+    [SerializeField] int index = 1;
 
-    [SerializeField]private Dictionary<int, float> miDiccionario = new Dictionary<int, float>();
+    [SerializeField] private TextAsset _textFile;
+    private Dictionary<int, float> miDiccionario = new Dictionary<int, float>();
 
-    // Start is called before the first frame update
+
     void Start()
     {
-        miDiccionario.Add(1, 5);
-        miDiccionario.Add(2, 7);
-        miDiccionario.Add(3, 9);
-        miDiccionario.Add(4, miDiccionario.Count);
-        
+        ConvertTxtToDictionary();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Timer+=Time.deltaTime;
-        if(Mathf.Floor(Timer)==5)
-        {
-            
-        }
+        Timer += Time.deltaTime;
+
         if(miDiccionario[index] == Mathf.Floor(Timer))
         {
             index++;
-            Instantiate (nota,transform.position,transform.rotation);
-            
+            Instantiate (nota,transform.position,transform.rotation);            
+        }
+    }
+
+    private void ConvertTxtToDictionary()
+    {
+        // Fijáte de que se haya asignado un archivo de texto en el Inspector
+        if (_textFile != null)
+        {
+            string[] lines = _textFile.text.Split('\n');
+
+            foreach (string line in lines)
+            {
+                string[] keyValue = line.Trim().Split(':');
+                if (keyValue.Length == 2)
+                {
+                    int key = int.Parse(keyValue[0].Trim());
+                    float value = float.Parse(keyValue[1].Trim());
+                    miDiccionario[key] = value;
+                }
+            }
+
+            miDiccionario[miDiccionario.Count + 1] = -1;
+
+            foreach (KeyValuePair<int, float> kvp in miDiccionario)
+            {
+                Debug.Log("Clave: " + kvp.Key + ", Valor: " + kvp.Value);
+            }
+        }
+        else
+        {
+            Debug.LogError("No se asignó un archivo de texto.");
         }
     }
 }
