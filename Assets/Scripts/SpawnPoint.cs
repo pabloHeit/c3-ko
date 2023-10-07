417874e6602f4987626ccaa12f3cbaa369d91a79
pabloHeit <pabloaheit@yahoo.com.ar>
2023-10-07 13:35:51 -0300

    Sistema de 4 tipos de notas
    El script toma un archivo .txt que tiene como valores "el tipo de nota", "el tiempo en que se spawnea". En el sistema funciona como un diccionario[index] = (tipoNota, tiempoNota)

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
 
-
-
 public class SpawnPoint : MonoBehaviour
 {
     [SerializeField] GameObject nota;
@@ -12,7 +10,7 @@ public class SpawnPoint : MonoBehaviour
     [SerializeField] int index = 1;
 
     [SerializeField] private TextAsset _textFile;
-    private Dictionary<int, float> miDiccionario = new Dictionary<int, float>();
+    private Dictionary<int, (int, float)> miDiccionario = new Dictionary<int, (int, float)>();
 
 
     void Start()
@@ -24,11 +22,35 @@ public class SpawnPoint : MonoBehaviour
     {
         Timer += Time.deltaTime;
 
-        if(miDiccionario[index] == Mathf.Floor(Timer))
+        if(miDiccionario[index].Item2 == Mathf.Floor(Timer))
         {
-            index++;
-            Instantiate (nota,transform.position,transform.rotation);            
+            SpawnNote(miDiccionario[index].Item1);
         }
+    }
+
+    private void SpawnNote(int noteNum)
+    {
+        print($"Note: {noteNum}");
+
+        switch (noteNum)
+        {
+            case 1:
+                Instantiate(nota, transform.position, transform.rotation);
+                break;
+            case 2:
+                Instantiate(nota, transform.position, transform.rotation);
+                break;
+            case 3:
+                Instantiate(nota, transform.position, transform.rotation);
+                break;
+            case 4:
+                Instantiate(nota, transform.position, transform.rotation);
+                break;
+            default:
+                print($"Error: Valor de nota invalido: {noteNum}");
+                break;
+        }
+        index++;
     }
 
     private void ConvertTxtToDictionary()
@@ -36,22 +58,27 @@ public class SpawnPoint : MonoBehaviour
         // Fijáte de que se haya asignado un archivo de texto en el Inspector
         if (_textFile != null)
         {
+            //lines = [ ['noteColor, noteTime'] ]
             string[] lines = _textFile.text.Split('\n');
 
-            foreach (string line in lines)
+            for (int i = 0; i < lines.Length; i++)
             {
-                string[] keyValue = line.Trim().Split(':');
+                int keyQuantity = lines.Length;
+                string[] keyValue = lines[i].Trim().Split(',');
+
                 if (keyValue.Length == 2)
                 {
-                    int key = int.Parse(keyValue[0].Trim());
-                    float value = float.Parse(keyValue[1].Trim());
-                    miDiccionario[key] = value;
+                    int noteColor = int.Parse(keyValue[0].Trim());
+                    float noteTime = float.Parse(keyValue[1].Trim());
+
+                    miDiccionario[i+1] = (noteColor, noteTime);
                 }
             }
 
-            miDiccionario[miDiccionario.Count + 1] = -1;
+            miDiccionario[miDiccionario.Count + 1] = (-1, -1);
 
-            foreach (KeyValuePair<int, float> kvp in miDiccionario)
+            Debug.Log("Notas musicales: ");
+            foreach (KeyValuePair<int, (int, float)> kvp in miDiccionario)
             {
                 Debug.Log("Clave: " + kvp.Key + ", Valor: " + kvp.Value);
             }
